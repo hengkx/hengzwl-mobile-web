d2aaf6713cfaaf23cfdc592fce6abefd632109ca
Tom Xu <ycxzhkx@gmail.com>
2021-01-07 21:55:50 +0800

    Update Invite.tsx

--- src/components/wechat/Invite.tsx
+++ src/components/wechat/Invite.tsx
@@ -25,7 +25,7 @@ const Invite: React.FC = () => {
       <img className="logo" src={logo} alt="logo" />
       <Space direction="vertical" align="center">
         <div>好友 {name}</div>
-        <div style={{ fontSize: 18 }}>送你7天恒记VIP</div>
+        <div style={{ fontSize: 18 }}>邀您体验 恒记 APP</div>
       </Space>
       <Button block type="primary" size="large" style={{ marginTop: 50 }} onClick={handleReceive}>
         立即领取

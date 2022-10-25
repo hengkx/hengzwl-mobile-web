f285788d3c70909b6a2d943a4d076e5c360588d1
Tom Xu <ycxzhkx@gmail.com>
2022-10-25 19:17:48 +0800

    imrpove

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import * as React from 'react';
 import logo from '../../images/logo.png';
 import './less/main.less';
@@ -6,9 +7,13 @@ const Main: React.FC = () => (
   <div className="main">
     <img className="logo" src={logo} alt="logo" />
     <div className="title">恒记</div>
-    <a className="btn" href="https://a.app.qq.com/o/simple.jsp?pkgname=com.hengzwl.privacy">
-      下载APP
+    <a className="btn" href="https://itunes.apple.com/cn/app/id1523682629">
+      苹果下载
     </a>
+    <span className="btn">安卓稍后提供</span>
+    {/* <a className="btn" href="https://a.app.qq.com/o/simple.jsp?pkgname=com.hengzwl.privacy">
+      下载APP
+    </a> */}
   </div>
 );
 

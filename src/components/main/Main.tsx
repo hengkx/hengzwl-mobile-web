e8580a044aa169380b089c1c9036e5a5238b47e2
Tom Xu <ycxzhkx@gmail.com>
2022-10-31 18:18:33 +0800

    Update Main.tsx

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -10,7 +10,13 @@ const Main: React.FC = () => (
     <a className="btn" href="https://itunes.apple.com/cn/app/id1523682629">
       苹果下载
     </a>
-    <span className="btn">安卓稍后提供</span>
+    <a
+      className="btn"
+      href="http://file.market.xiaomi.com/download/AppStore/00154380655a2474e8d5db35fd17d922e2b9129db/com.hengzwl.privacy_1.9.51.apk"
+    >
+      安卓下载已在小米平台上架
+    </a>
+    <div>已测试在小米手机上可以正常收到通知</div>
     {/* <a className="btn" href="https://a.app.qq.com/o/simple.jsp?pkgname=com.hengzwl.privacy">
       下载APP
     </a> */}

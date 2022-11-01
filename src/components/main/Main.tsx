5656ef9836526f8f96b29a00cc1ccf47b932dbbd
Tom Xu <ycxzhkx@gmail.com>
2022-11-01 20:08:07 +0800

    Update Main.tsx

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,26 +1,35 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
+import axios from 'axios';
 import * as React from 'react';
 import logo from '../../images/logo.png';
 import './less/main.less';
 
-const Main: React.FC = () => (
-  <div className="main">
-    <img className="logo" src={logo} alt="logo" />
-    <div className="title">恒记</div>
-    <a className="btn" href="https://itunes.apple.com/cn/app/id1523682629">
-      苹果下载
-    </a>
-    <a
-      className="btn"
-      href="http://file.market.xiaomi.com/download/AppStore/00154380655a2474e8d5db35fd17d922e2b9129db/com.hengzwl.privacy_1.9.51.apk"
-    >
-      安卓下载已在小米平台上架
-    </a>
-    <div>已测试在小米手机上可以正常收到通知</div>
-    {/* <a className="btn" href="https://a.app.qq.com/o/simple.jsp?pkgname=com.hengzwl.privacy">
-      下载APP
-    </a> */}
-  </div>
-);
+const Main: React.FC = () => {
+  const [data, setData] = React.useState<any>({});
+
+  React.useEffect(() => {
+    // eslint-disable-next-line promise/catch-or-return, promise/always-return
+    axios.get('https://api.privacy.hengzwl.com/api/version').then((res) => {
+      setData(res.data);
+    });
+  }, []);
+
+  return (
+    <div className="main">
+      <img className="logo" src={logo} alt="logo" />
+      <div className="title">恒记{data.version}</div>
+      <a className="btn" href="https://itunes.apple.com/cn/app/id1523682629">
+        苹果下载
+      </a>
+      <a className="btn" href={data.downloadUrl}>
+        安卓下载已在小米平台上架
+      </a>
+      <div>{data.description}</div>
+      {/* <a className="btn" href="https://a.app.qq.com/o/simple.jsp?pkgname=com.hengzwl.privacy">
+        下载APP
+      </a> */}
+    </div>
+  );
+};
 
 export default Main;

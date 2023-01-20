e96e5baffddc7585e908f1c2132031bc5532d178
Tom Xu <ycxzhkx@gmail.com>
2023-01-20 23:23:18 +0800

    improve

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import React, { FC } from 'react';
-import useSWR from 'swr';
 import Image from 'next/image';
 
 export interface CheckUpdate {
@@ -14,25 +13,44 @@ export interface CheckUpdate {
   ApkMd5: string;
 }
 
-const Main: FC = () => {
-  const { data } = useSWR<CheckUpdate>('https://api.privacy.hengzwl.com/api/checkUpdate');
+interface MainProps {
+  data: CheckUpdate;
+}
 
+const Main: FC<MainProps> = ({ data }) => {
   return (
     <div className="h-full flex flex-col items-center justify-center -mt-8 gap-4">
-      <Image src="/logo.png" width={64} height={64} alt="logo" className="rounded-lg" />
-      <div className="text-xl">恒记{data?.VersionName}</div>
+      <Image
+        src="/logo.png"
+        width={64}
+        height={64}
+        alt="logo"
+        className="rounded-lg"
+      />
+      <div className="text-xl">恒记{data.VersionName}</div>
       <a
         className="bg-black text-white w-60 py-2 text-center rounded-md"
         href="https://itunes.apple.com/cn/app/id1523682629"
       >
         苹果下载
       </a>
-      <a className="bg-black text-white w-60 py-2 text-center rounded-md" href={data?.DownloadUrl}>
+      <a
+        className="bg-black text-white w-60 py-2 text-center rounded-md"
+        href={data.DownloadUrl}
+      >
         安卓下载已在小米平台上架
       </a>
-      <pre>{data?.ModifyContent}</pre>
+      <pre>{data.ModifyContent}</pre>
     </div>
   );
 };
 
 export default Main;
+
+export async function getStaticProps() {
+  const res = await fetch('https://api.privacy.hengzwl.com/api/checkUpdate');
+
+  return {
+    props: { data: await res.json() },
+  };
+}

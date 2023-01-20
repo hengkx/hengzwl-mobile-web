f95656d7c096ddf3f49c7574dec865379b1a51c9
Tom Xu <ycxzhkx@gmail.com>
2023-01-20 23:06:01 +0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,39 +15,53 @@ axios.interceptors.request.use(
     config.headers.set('Authorization', localStorage.getItem('token'));
     return config;
   },
-  (error) => error,
+  (error) => error
 );
 axios.interceptors.response.clear();
 axios.interceptors.response.use(
   ({ data }) => {
-    if (data && data.code === 401 && window.location.pathname !== '/account/login') {
+    if (
+      data &&
+      data.code === 401 &&
+      window.location.pathname !== '/account/login'
+    ) {
       return (window.location.pathname = '/account/login');
     }
     return data;
   },
   (error) => {
     console.log(error);
-    if (error.response.status === 401 && !window.location.pathname.startsWith('/account/login')) {
+    if (
+      error.response.status === 401 &&
+      !window.location.pathname.startsWith('/account/login')
+    ) {
       Router.push(`/account/login?redirect=${window.location.pathname}`);
       return;
     }
     return { code: 500, message: error.message };
-  },
+  }
 );
 
 interface MyAppProps extends AppProps {
   isDarkMode: 'dark' | 'light';
 }
 
-export default function MyApp({ Component, pageProps, isDarkMode }: MyAppProps) {
+export default function MyApp({
+  Component,
+  pageProps,
+  isDarkMode,
+}: MyAppProps) {
   const router = useRouter();
 
   return (
     <>
       <Head>
-        <title>恒记APP-游戏·记账·事件·文件</title>
+        <title>恒记APP</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="keywords" content="恒记APP,彩虹岛,数据查询,装备查询,彩虹岛升级材料,装备对比" />
+        <meta
+          name="keywords"
+          content="恒记APP下载,彩虹岛,数据查询,装备查询,彩虹岛升级材料,装备对比"
+        />
         <meta
           name="description"
           content="恒记是一款提升工作和生活中效率的 APP。彩虹岛等一些游戏装备、怪物数据等一些数据查询，支持装备对比，订阅官网更新通知，游戏开服通知。同时还支持记账、事件提醒、已经智能Twitter文件下载等。"

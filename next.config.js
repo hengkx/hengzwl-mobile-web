85256a711585c3360cb11952706675b434328d67
Tom Xu <ycxzhkx@gmail.com>
2023-12-06 12:14:27 +0800

    improve

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  typedRoutes: true,
   reactStrictMode: true,
   transpilePackages: [
     '@babel/runtime',
--- pages/account/[id].tsx
+++ pages/account/[id].tsx
@@ -2,24 +2,49 @@ import React from 'react';
 import axios from 'axios';
 import useSWR from 'swr';
 import { useParams, useRouter } from 'next/navigation';
-import { Button, Table, Typography } from 'antd';
+import { Button, Table, Tabs, TabsProps, Typography } from 'antd';
 import dayjs from 'dayjs';
 import { useFetch } from '@/hooks';
 
 const { Paragraph, Text } = Typography;
 
+function Item(props: any) {
+  return (
+    <div className="grid grid-cols-4 gap-2 h-full overflow-y-auto">
+      {props.items.map((item: any, index: number) => (
+        <div key={index}>
+          <Text className="block">
+            {item.type} {item.name || item.id} x{item.count}
+          </Text>
+          {/* {item.enchants.map((p: any) => (
+            <Text className="block" type="secondary" key={p.id}>
+              {p.description}
+            </Text>
+          ))} */}
+        </div>
+      ))}
+    </div>
+  );
+}
+
 function Detail() {
   const router = useRouter();
-  const { id } = useParams();
+  const { id } = useParams() || {};
 
   const { data } = useFetch<AccountInfo>(id && `/api/chd/info/${id}`);
   if (!data) {
     return;
   }
 
+  const items: TabsProps['items'] = data.packages.map((p) => ({
+    key: p.type,
+    label: `${p.type}[${p.count}]`,
+    children: <Item items={p.items} />,
+  }));
+
   return (
-    <div className="h-full overflow-hidden flex gap-4 py-4 px-4">
-      <div>
+    <div className="h-full overflow-hidden flex gap-4 px-4">
+      <div className="py-4">
         <Text strong>
           激活套装：{data.collectSetCount} 激活数：{data.collectCount}
         </Text>
@@ -36,15 +61,7 @@ function Detail() {
           </div>
         ))}
       </div>
-      <div className="grid grid-cols-4 gap-2 h-full overflow-y-auto">
-        {data.packages.map((item, index) => (
-          <div key={index}>
-            <Text>
-              {item.type} {item.name || item.id} x{item.count}
-            </Text>
-          </div>
-        ))}
-      </div>
+      <Tabs defaultActiveKey="1" items={items} style={{ height: '100%' }} />
     </div>
   );
 }

1ceb70f312186f1c989d945caeb864b751a82ac8
Tom Xu <ycxzhkx@gmail.com>
2024-04-15 21:14:08 +0800

    improve

--- pages/article/preview/[id].tsx
+++ pages/article/preview/[id].tsx
@@ -7,6 +7,9 @@ import { Article } from '..';
 import { useParams } from 'next/navigation';
 import { useRouter } from 'next/router';
 import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+
+dayjs.extend(relativeTime);
 
 const { Text } = Typography;
 
@@ -21,23 +24,24 @@ function ArticlePage() {
   const router = useRouter();
 
   return (
-    <div className="h-screen p-4 flex flex-col gap-4">
+    <div className="article h-screen p-4 flex flex-col gap-4">
       <Skeleton loading={!data}>
         {data && (
           <div className="flex flex-col gap-4">
             <div className="text-xl">{data.title}</div>
-            <div className="flex items-center gap-1">
+            <div className="flex items-center gap-2">
               <Avatar
                 src={
-                  !data.user.avatar.startsWith('http')
-                    ? 'https://oss.hengzwl.com/'
-                    : '' + data.user.avatar
+                  (!data.user.avatar.startsWith('http') ? 'https://oss.hengzwl.com/' : '') +
+                  data.user.avatar
                 }
               />
-              <Text style={{ fontSize: 16 }}>{data.user.name}</Text>
-              <Text style={{ fontSize: 16 }} type="secondary">
-                {dayjs(data.createdAt).format('YYYY-MM-DD HH:mm:ss')}
-              </Text>
+              <div className="flex flex-col gap-1">
+                <div style={{ fontSize: 14, lineHeight: 1 }}>{data.user.name}</div>
+                <Text style={{ fontSize: 12, lineHeight: 1 }} type="secondary">
+                  {dayjs(data.createdAt).fromNow()}
+                </Text>
+              </div>
             </div>
             <div className="" dangerouslySetInnerHTML={{ __html: data.html }} />
           </div>
